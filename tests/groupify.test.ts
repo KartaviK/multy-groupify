9e9707db793f217d35de2db6dcf61d40b8208881
KartaviK <varkuta.roma@gmail.com>
2019-11-20 13:52:28 +0200

    Add some tests to up coverage

--- tests/groupify.test.ts
+++ tests/groupify.test.ts
@@ -10,24 +10,48 @@ const simpleCollection = [
 
 describe("Groupify by callbacks!", () => {
     it("Group by only one attribute", () => {
-        const groupsByName = groupify(simpleCollection, {
+        const byAge = groupify(simpleCollection, {
             age: (value: any) => value
         });
 
-        expect(groupsByName.length).toBe(3);
-        expect(groupsByName[0].Keys.age).toBe(12);
-        expect(groupsByName[1].Keys.age).toBe(6);
-        expect(groupsByName[2].Keys.age).toBe(16);
+        expect(byAge.length).toBe(3);
+        expect(byAge[0].Keys.age).toBe(12);
+        expect(byAge[1].Keys.age).toBe(6);
+        expect(byAge[2].Keys.age).toBe(16);
+    });
+
+    it('Group with simple custom compare callback', () => {
+        const byAge = groupify(simpleCollection, {
+            age: (value: any) => value
+        }, {
+            age: (groupValue, compareValue) => groupValue === compareValue
+        });
+
+        expect(byAge.length).toBe(3);
+    });
+
+    it('Group with custom specific compare callback', () => {
+        const byAge = groupify(simpleCollection, {
+            age: (value: any) => value
+        }, {
+            age: (groupValue, compareValue) => groupValue === compareValue && compareValue === 16
+        });
+
+        expect(byAge.length).toBe(4);
+        expect(byAge[0].Keys.age).toBe(12);
+        expect(byAge[1].Keys.age).toBe(12);
+        expect(byAge[2].Keys.age).toBe(6);
+        expect(byAge[3].Keys.age).toBe(16);
     });
 });
 
 describe("Groupify by simple array of properties names!", () => {
     it("Group by only one attribute", () => {
-        const groupsByName = groupify(simpleCollection, ['age']);
+        const byAge = groupify(simpleCollection, ['age']);
 
-        expect(groupsByName.length).toBe(3);
-        expect(groupsByName[0].Keys.age).toBe(12);
-        expect(groupsByName[1].Keys.age).toBe(6);
-        expect(groupsByName[2].Keys.age).toBe(16);
+        expect(byAge.length).toBe(3);
+        expect(byAge[0].Keys.age).toBe(12);
+        expect(byAge[1].Keys.age).toBe(6);
+        expect(byAge[2].Keys.age).toBe(16);
     });
 });
