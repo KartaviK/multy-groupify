6af2780dd84241057afc05ef92a73a0fdf4a7be6
KartaviK <varkuta.roma@gmail.com>
2019-11-20 06:33:44 +0200

    Upgrade logic to use simple array of attributes instead of callbacks

--- tests/groupify.test.ts
+++ tests/groupify.test.ts
@@ -1,22 +1,35 @@
-import groupify from '../src/groupify';
+import groupify from "../src/groupify";
 
-describe('Simple groupifying', () => {
-    it('Group by only one attribute', () => {
-        const collection = [
-            {name: 'Kasper', age: 12},
-            {name: 'Roman', age: 12},
-            {name: 'John', age: 6},
-            {name: 'Elena', age: 16},
-            {name: 'Urgant', age: 16},
-        ];
+const simpleCollection = [
+    {name: "Kasper", age: 12},
+    {name: "Roman", age: 12},
+    {name: "John", age: 6},
+    {name: "Elena", age: 16},
+    {name: "Urgant", age: 16},
+];
 
-        const groupsByName = groupify(collection, {
-            age: value => value,
+describe("Groupify by callbacks!", () => {
+    it("Group by only one attribute", () => {
+        const groupsByName = groupify(simpleCollection, {
+            retrieve: {
+                age: (value: any) => value
+            },
         });
 
         expect(groupsByName.length).toBe(3);
         expect(groupsByName[0].Keys.age).toBe(12);
         expect(groupsByName[1].Keys.age).toBe(6);
         expect(groupsByName[2].Keys.age).toBe(16);
-    })
+    });
+});
+
+describe("Groupify by simple array of properties names!", () => {
+    it("Group by only one attribute", () => {
+        const groupsByName = groupify(simpleCollection, {retrieve: ['age']});
+
+        expect(groupsByName.length).toBe(3);
+        expect(groupsByName[0].Keys.age).toBe(12);
+        expect(groupsByName[1].Keys.age).toBe(6);
+        expect(groupsByName[2].Keys.age).toBe(16);
+    });
 });
